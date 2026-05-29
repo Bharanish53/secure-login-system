0abf437f44b0545ef361521a9b132d583454f5a9
Bharanish53 <bharanish.pushparaj@gmail.com>
2026-05-29 13:51:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -112,5 +112,5 @@ app.get("/logout", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
